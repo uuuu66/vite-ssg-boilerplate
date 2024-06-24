ac5dff7c46282bf2b897b5372fc487a78432e8ca
notYellowMango <busimingi@gmail.com>
2024-06-24 14:41:44 +0900

    init. 프로젝트 초기 설정

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
+  theme: {
+    screens: {
+      "1200px": "1200px",
+      "780px": "780px",
+      "560px": "560px",
+      "360px": "360px",
+    },
+  },
+  plugins: [],
+};
